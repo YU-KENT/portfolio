27a6dc1efde35e761259644d9e569ce78e0661b3
YU KENT <tianyu.tina@gmail.com>
2023-10-13 10:46:33 +0200

    add scroll navbar

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,55 @@ listItems.forEach(function (li) {
     }
   });
 });
+
+
+function adjustAnnimationSize() {
+  const containerWidth = document.querySelector('.animation-container').offsetWidth;
+  const container = document.querySelector('.animation-container')
+  const elementBird = document.querySelector('.bird');
+  const elementMosquito= document.querySelector('.mosquito');
+  const elementCloudRight = document.querySelector('.clouds.right')
+  const elementCloudLeft = document.querySelector('.clouds.left')
+  const elementCloudStyle2 = document.querySelector('.clouds.style2')
+  
+  // Adjust the size based on the container width
+  const newProportion = containerWidth / 1440; // You can adjust this value as needed
+  const regProportion = 300/1440;
+  container.style.height = regProportion*containerWidth + 'px';
+  console.log(container,"gg",containerWidth,)
+  elementBird.style.width = 100*newProportion + 'px';
+  elementMosquito.style.width = 25*newProportion + 'px';
+  elementCloudRight.style.width = 80*newProportion + 'px';
+  elementCloudLeft.style.width = 65*newProportion + 'px';
+  elementCloudStyle2.style.width = 65*newProportion + 'px';
+}
+
+// Add an event listener to adjust the size when the window is resized
+window.addEventListener('resize', adjustAnnimationSize);
+
+// Call the function initially
+adjustAnnimationSize();
+
+const navbarHidden = document.querySelector('.navbar.hidden');
+let prevScrollPos = window.scrollY;
+
+window.onscroll = function() {
+  const currentScrollPos = window.scrollY;
+
+  if (prevScrollPos > currentScrollPos) {
+    navbarHidden.style.top = '0';
+  } else {
+    navbarHidden.style.top = '-128x'; // Adjust the height of your navbar
+  }
+
+  prevScrollPos = currentScrollPos;
+};
+
+// Display the navbar when scrolling down
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 100) { // Adjust the scroll position at which the navbar appears
+    navbarHidden.style.display = 'flex';
+  } else {
+    navbarHidden.style.display = 'none';
+  }
+});
--- scripts/page.js
+++ scripts/page.js
@@ -19,6 +19,9 @@ const profilHTML= `
         <div class="animation-container">
             <img class="bird" src="./asserts/bird.svg" alt="Flying Bird">
             <img class="mosquito" src="./asserts/mosquito.svg" alt="Flying mosquito">
+            <img class="clouds left" src="./asserts/cloud.svg" alt="cloud photo">
+            <img class="clouds left style2" src="./asserts/cloud2.svg" alt="cloud photo">
+            <img class="clouds right" src="./asserts/cloud.svg" alt="cloud photo">
         </div>
         <div class="profil">
             <div class="profil_image">
@@ -43,9 +46,7 @@ const profilHTML= `
                 </p>
             </div>
         </div>
-        <img class="cloud-left" src="./asserts/cloud.svg" alt="cloud photo">
-        <img class="cloud-left cloud2" src="./asserts/cloud2.svg" alt="cloud photo">
-        <img class="cloud-right" src="./asserts/cloud.svg" alt="cloud photo">
+        
                             `
 
 newdivprofil.innerHTML = profilHTML;
@@ -55,5 +56,79 @@ sectionprofil.appendChild(newdivprofil)
 const sectionCompetences = document.getElementById('competences')
 const newdivCompetences = document.createElement("div")
 const competencesHTML= `
-
+                    <div class="competences_title">
+                        <h2>MES COMPÉTENCES</h2>
+                        <h3>Des compétences à votre service</h3>
+                    </div>
+                    <div class="competences_contents">
+                       <div class="contents_left">
+                         <h3>Domaines de compétences</h3>
+                            <div class="content">
+                              <i class="fa-solid fa-circle-check" style="color: #0c2e6a;"></i>
+                              <h4>GESTION DE PROJETS WEB</h4>
+                            </div>
+                            <p>Site vitrine, intranet,corporate, évènementiel, e-commerce </p>
+                           
+                           <div class="content">
+                            <i class="fa-solid fa-circle-check" style="color: #0c2e6a;"></i>
+                             <h4>INTÉGRATION HTML / CSS</h4>
+                           </div>
+                           <p>Respectueux des standards du Web / Accessibilité </p>
+                          
+                           <div class="content" >
+                            <i class="fa-solid fa-circle-check" style="color: #0c2e6a;"></i>
+                            <h4>APPLICATIONS SPÉCIFIQUES ET INTERFACE D'ULTILISATEUR</h4>
+                           </div>
+                            <p>JAVASCRIPT - REACT - REDUX - API</p>
+                       
+                          <div class="content">
+                            <i class="fa-solid fa-circle-check" style="color: #0c2e6a;"></i>
+                            <h4>TEST UNITAIRE / INTÉGRATION D'APPLICATIONS</h4>
+                         </div>
+                            <p>JEST</p>
+                       </div class="content">
+                       <div class="contents_right">
+                          <h3>Compétences en développement</h3>
+                          <h4>FRONT-END</h4>
+                          <div class="bar">
+                            <span>HTML - CSS</span>
+                            <div class="data-bar">
+                                <div class="data-value" style="width: 90%;"></div>
+                            </div>
+                         </div>
+                         <div class="bar">
+                            <span>JAVASCRIPT</span>
+                            <div class="data-bar">
+                                <div class="data-value" style="width: 90%;"></div>
+                            </div>
+                         </div>
+                         <div class="bar">
+                            <span>REACT</span>
+                            <div class="data-bar">
+                                <div class="data-value" style="width: 80%;"></div>
+                            </div>
+                         </div>
+                         <div class="bar">
+                            <span>ANGULAR</span>
+                            <div class="data-bar">
+                                <div class="data-value" style="width: 25%;"></div>
+                            </div>
+                         </div>
+                         <div class="bar">
+                            <span>TEST-JEST</span>
+                            <div class="data-bar">
+                                <div class="data-value" style="width: 60%;"></div>
+                            </div>
+                         </div>
+                         <h4>BACK-END</h4>
+                         <div class="bar">
+                            <span>MONGODB / EXPRESS</span>
+                            <div class="data-bar">
+                                <div class="data-value" style="width: 60%;"></div>
+                            </div>
+                         </div>
+                       </div>
+                    </div>
 `
+newdivCompetences.innerHTML = competencesHTML ;
+sectionCompetences.appendChild(newdivCompetences);
